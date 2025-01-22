a4847f90b6c8aa477692ad99c139a04458c70d09
igor <93474580+lgor360@users.noreply.github.com>
2025-01-22 13:33:04 +0300

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,26 @@
-async function readPost(postid) {
-    const response = await;
+const api = `https://${localStorage.getItem("accountServer")}/api/v3`
+const lemmyToken = localStorage.getItem("lemmyToken");
+
+async function upOrDownPost(like, type, id) {
+    try {
+        const response = await fetch("https://oldemmi.vercel.app/api/server.js", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+                url: `https://${server}/api/v3/post/like`,
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": `Bearer ${lemmyToken}`
+                },
+                body: {
+                    "score": like,
+                    `${type}_id`: id
+                }
+            })
+        });
+        console.log(JSON.stringify(await response.json(), null, 2));
+    } catch (error) {
+        alert(`action failed: ${error}`);
+    }
 }
