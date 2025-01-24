809b13a673e25e5906a5651a984aca42ee4533dd
igor <93474580+lgor360@users.noreply.github.com>
2025-01-24 20:25:39 +0300

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,7 +1,16 @@
-function parseMarkdown(text) {
-    const marktextgotovo = fetch("https://oldemmi.vercel.app/api/markApi.js", {
+async function parseMarkdown(text) {
+    const response = await fetch("https://oldemmi.vercel.app/api/server.js", {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({ marktext: text })
     });
+
+    if (!response.ok) {
+        throw new Error(`server returned error: ${response.status}`);
+    }
+
+    const marktextgotovo = await response.text(); // получаем текст
     return marktextgotovo;
 }
