f70acca3f826defdfe711578f8d774069cc0f9d7
igor <93474580+lgor360@users.noreply.github.com>
2025-01-29 00:25:33 +0300

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,7 +1,13 @@
 async function parseMarkdown(text) {
-    const scriptmark = document.createElement("script");
-    scriptmark.src = "https://unpkg.com/showdown/dist/showdown.min.js";
-    document.head.appendChild(scriptmark);
-    var md = new showdown.Converter();
-    return md.makeHtml(`${text}`);
+  const scriptmark = document.createElement("script");
+  scriptmark.src = "https://unpkg.com/showdown/dist/showdown.min.js";
+
+  // возвращаем Promise, который выполнится после загрузки скрипта
+  await new Promise((resolve, reject) => {
+    scriptmark.onload = resolve;
+    scriptmark.onerror = reject;
+  });
+
+  var md = new showdown.Converter();
+  return md.makeHtml(`${text}`);
 }
