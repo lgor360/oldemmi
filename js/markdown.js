ef0bf2fe069f33719619808926b11c508a43074c
igor <93474580+lgor360@users.noreply.github.com>
2025-01-29 00:08:04 +0300

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,5 +1,5 @@
 async function parseMarkdown(text) {
-    const response = await fetch("https://oldemmi.vercel.app/api/markApi.js", {
+    const response = await fetch("https://oldemmi.vercel.app/convert", {
         method: "POST",
         headers: {
             "content-type": "application/json"
