39853efd3ea9a53d3295673a9c7a0e8d2e27f47c
igor <93474580+lgor360@users.noreply.github.com>
2025-01-29 00:21:12 +0300

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,7 +1,7 @@
 async function parseMarkdown(text) {
     const scriptmark = document.createElement("script");
-    scriptmark.src = "https://cdn.jsdelivr.net/remarkable/1.7.1/remarkable.min.js";
+    scriptmark.src = "https://unpkg.com/showdown/dist/showdown.min.js";
     document.head.appendChild(scriptmark);
-    var md = new Remarkable();
-    return md.render(`${text}`);
+    var md = new showdown.Converter();
+    return md.makeHtml(`${text}`);
 }
