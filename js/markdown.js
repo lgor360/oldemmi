d4a9ba8a55dcb90680661a1df1d68c098e0fa6c5
igor <93474580+lgor360@users.noreply.github.com>
2025-02-09 16:17:23 +0300

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -7,42 +7,51 @@ function parseMarkdown(text) {
     });
     // объединение соседних списков на одном уровне
     text = text.replace(/<\/(ul|ol)>\s*<\1>/g, '');
+
     // чекбоксы
-    text = text.replace(/\[ \]\s(.+)/g, '<input type="checkbox" disabled>$1');
-    text = text.replace(/\[x\]\s(.+)/gi, '<input type="checkbox" checked disabled>$1');
-    // подчёркнутый текст
-    text = text.replace(/__([^_]+)__/g, '<u>$1</u>');
-    // жирный текст (*текст*)
-    text = text.replace(/\*{2}([^\*]+)\{2}/g, '<strong>$1</strong>');
-    // курсив (_текст_)
-    text = text.replace(/\_([^\*]+)\_/g, '<em>$1</em>');
-    // зачёркнутый текст (~~текст~~)
+    text = text.replace(/\[ \]\s(.+)/g, '<input type="checkbox" disabled> $1');
+    text = text.replace(/\[x\]\s(.+)/gi, '<input type="checkbox" checked disabled> $1');
+
+    // жирный текст **жирный**
+    text = text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+
+    // курсив _курсив_
+    text = text.replace(/_(.*?)_/g, '<em>$1</em>');
+
+    // зачёркнутый текст ~~зачеркнутый~~
     text = text.replace(/~~(.*?)~~/g, '<del>$1</del>');
-    // цитаты (> текст)
-    text = text.replace(/^>\s(.+)/gm, '<blockquote>”$1”</blockquote>');
-    // код (`текст`)
-    text = text.replace(/`([^`]+)`/g, '<code>$1</code>');
+
     // прописные (~текст~)
     text = text.replace(/~(.*?)~/g, '<sub>$1</sub>');
+
     // заглавные (^текст^)
     text = text.replace(/\^(.*?)\^/g, '<sup>$1</sup>');
-    // спойлеры (::spoiler текст:::)
+
+    // цитаты > текст
+    text = text.replace(/^>(.*)$/gm, '<blockquote>$1</blockquote>');
+
+    // код `код`
+    text = text.replace(/`([^`]+)`/g, '<code>$1</code>');
+
+    // спойлеры ::: spoiler Заголовок\nТекст\n:::
     text = text.replace(/::: spoiler (.+?)\n([\s\S]+?)\n:::/g, 
         '<details><summary>$1</summary><div>$2</div></details>');
-    // картинки
+
+    // изображения ![alt](url)
     text = text.replace(/!\[([^\]]*)\]\((https?:\/\/[^\)]+)\)/g, '<br><img src="$2" alt="$1" style="max-width: 100%; height: auto;"><br>');
-    // ссылки ([текст](ссылка))
+
+    // ссылки [текст](ссылка)
     text = text.replace(/\[([^\]]+)\]\((https?:\/\/[^\)]+)\)/g, '<a href="$2" rel="noopener noreferrer" target="_blank">$1</a>');
-    // заголовки
+
+    // заголовки # Заголовок
     text = text.replace(/^(#+)\s*(.+)$/gm, (_, hashes, content) => `<h${hashes.length}>${content}</h${hashes.length}>`);
-    // переносы строк
-    text = text.replace(/\n\n/g, '<br><br>');
-    text = text.replace(/\\\n/g, "<br>");
+
     // обработка ссылок типа !сообщество@инстанс
     text = text.replace(/!([\w\d_]+)@([\w\d\.-]+)/g, (match, community, instance) => {
         const url = `https://oldemmi.vercel.app/community?server=${instance}&community=${community}`;
         return `<a href="${url}" rel="noopener noreferrer">${community}@${instance}</a>`;
     });
+
     // таблицы
     text = text.replace(/^\|(.+?)\|\n\|[-\s|]+\|\n((?:\|.*?\|(?:\n|$))*)/gm, (_, headers, rows) => {
         const headerCells = headers.split('|').map(cell => `<th>${cell.trim()}</th>`).join('');
@@ -53,5 +62,9 @@ function parseMarkdown(text) {
         return `<table><thead><tr>${headerCells}</tr></thead><tbody>${bodyRows}</tbody></table>`;
     });
 
-    return text;
+    // переносы строк
+    text = text.replace(/\n\n/g, '<br><br>'); // разделение абзацев
+    text = text.replace(/\\\n/g, "<br>"); // обычный перенос строки
+
+    return text; // оборачиваем в параграф
 }
