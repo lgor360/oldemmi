30458bcacb66626f5c4cf2b2c6734dccc1714c89
igor <93474580+lgor360@users.noreply.github.com>
2025-05-28 22:38:50 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,7 +12,9 @@ module.exports = async (req, res) => {
     }
 
     if (req.method !== 'POST') {
-        return res.status(405).json({ error: "Method not allowed" });
+        if (req.method !== 'GET') {
+            return res.status(405).json({ error: "Method not allowed" });
+        }
     }
 
     const { url, method = 'GET', headers, body } = req.body;
