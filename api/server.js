5feac68b6c61385c05c365b7e5fcdc1f0716f25d
igor <93474580+lgor360@users.noreply.github.com>
2025-05-29 10:00:24 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,47 +2,66 @@ const fetch = require("node-fetch");
 const url = require("url");
 
 module.exports = async (req, res) => {
-    // добавляем CORS-заголовки для разрешения всего
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+  // добавляем CORS-заголовки для разрешения всего
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
-    // обрабатываем preflight-запросы (OPTIONS)
-    if (req.method === 'OPTIONS') {
-        return res.status(204).end(); // успешный ответ для preflight
+  // обрабатываем preflight-запросы (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end(); // успешный ответ для preflight
+  }
+
+  if (req.method!== 'POST') {
+    if (req.method!== 'GET') {
+      return res.status(405).json({ error: "Method not allowed" });
     }
+  }
 
-    if (req.method!== 'POST') {
-        if (req.method!== 'GET') {
-            return res.status(405).json({ error: "Method not allowed" });
-        }
-    }
+  let requestUrl, method, headers, body, need;
 
-    let { url: requestUrl, method = 'GET', headers, body, need = "json"} = req.body;
-
-    if (req.method === 'GET') {
-        const urlParts = url.parse(req.url, true);
-        requestUrl = urlParts.href;
-        method = 'GET';
-        headers = {};
-        body = {};
-        for (const key in urlParts.query) {
-            if (urlParts.query.hasOwnProperty(key)) {
-                body[key] = urlParts.query[key];
-            }
-        }
-    }
-
-    try {
-        const response = await fetch(requestUrl, { method, headers, body: JSON.stringify(body) });
-        if (need == "text") {
-            const data = await response.text();
-            res.status(response.status).send(data);
+  if (req.method === 'GET') {
+    const urlParts = url.parse(req.originalUrl, true);
+    requestUrl = urlParts.href;
+    method = 'GET';
+    headers = {};
+    body = {};
+    need = 'json';
+    for (const key in req.query) {
+      if (req.query.hasOwnProperty(key)) {
+        if (key === 'need') {
+          need = req.query[key];
+        } else if (key === 'url') {
+          requestUrl = req.query[key];
         } else {
-            const data = await response.json();
-            res.status(response.status).json(data);
+          body[key] = req.query[key];
         }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+      }
     }
+  } else {
+    try {
+      const requestBody = JSON.parse(req.rawBody);
+      requestUrl = requestBody.url;
+      method = requestBody.method || 'GET';
+      headers = requestBody.headers || {};
+      body = requestBody.body || {};
+      need = requestBody.need || 'json';
+    } catch (error) {
+      res.status(400).json({ error: "Invalid request body" });
+      return;
+    }
+  }
+
+  try {
+    const response = await fetch(requestUrl, { method, headers, body: JSON.stringify(body) });
+    if (need === "text") {
+      const data = await response.text();
+      res.status(response.status).send(data);
+    } else {
+      const data = await response.json();
+      res.status(response.status).json(data);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
